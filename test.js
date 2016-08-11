8ec93e22817bbcc6ae041c63d9c5508b300c40c3
davidsawyer-hound <davidsawyer10+githubhound@gmail.com>
2016-08-10 21:58:12 -0400

    Add some bad code

--- test.js
+++ test.js
@@ -6,3 +6,22 @@ function getPluginName() {
 }
 
 getPluginName();
+
+var pluginName = "anothername",
+    defaults = {
+        propertyName: "value",
+    };
+
+function Plugin ( element, options ) {
+    this.element = element;
+    this.settings = $.extend( {}, defaults, options );
+    this._defaults = defaults;
+    this._name = pluginName;
+    this.init();
+}
+
+$.extend( Plugin.prototype, {
+    init: function() {
+        this.yourOtherFunction( "jQuery Boilerplate" );
+    }
+});
